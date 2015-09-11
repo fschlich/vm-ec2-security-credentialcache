2f965a4d7f390bba15b708f32c9ea8eab8d0ceb8
Florian Schlichting <fsfs@debian.org>
2015-09-11 21:19:02 +0200

    fix typo

--- lib/VM/EC2/Security/CredentialCache.pm
+++ lib/VM/EC2/Security/CredentialCache.pm
@@ -22,7 +22,7 @@ Retrieves the current EC2 instance's IAM credentials and caches them until they 
 
 This module provides a cache for an EC2's IAM credentials represented by L<VM::EC2::Security::Credentials>. 
 Rather than retriving the credentials for every possible call that uses them, cache them until they
-expire and retreive them again if they have expired.
+expire and retrieve them again if they have expired.
 
 =cut
 
